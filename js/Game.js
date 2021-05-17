e36d89425482688bb52488a8152fba4e6bd1035b
sjpriya <72385035+sjpriya@users.noreply.github.com>
2021-05-17 20:25:56 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -34,24 +34,6 @@ class Game {
     text("Game Start", 120, 100)
     Player.getPlayerInfo();
 
-    if(allPlayers !== undefined){
-      var display_position = 130;
-      for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
-
-        display_position+=20;
-        // display_position = display_position + 20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
-      }
-    }
-
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=50
-      player.update();
-    }
+    
   }
 }
